31bdf9739fa9f6d48c3522770ca124d44190a4d6
decoezz <45102309+decoezz@users.noreply.github.com>
2018-11-16 18:06:15 +0200

    Add files via upload

---
+++ bot (1).js
@@ -0,0 +1,17 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${ᴰᴬᴿᴷ |ɖɛƈơ_ɛʑʑ#7717}!`);
+client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setStatus("love")
+});
+
+
+
+client.login(MzMwODM0OTA2NzgxNDUwMjQw.Dstdrg.gFpsSna59zUcaibc625uH56Os7Y);// لا تغير فيها شيء
